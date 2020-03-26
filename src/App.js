6320b0ad9c0560b125c9f4e98058de87cc188b58
Leonidas Yopan <leonidasyopan@gmail.com>
2020-03-26 12:53:27 -0300

    Started the Frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './Header';
+
+// JSX (JavaScript XML)
+
+function App() {
+	return (
+		<Header>
+			Semana Omnistack  
+		</Header>
+	);
+}
+
+export default App;
